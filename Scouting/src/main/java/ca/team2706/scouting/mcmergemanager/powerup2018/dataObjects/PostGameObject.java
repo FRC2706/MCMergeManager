a7bd4861e038d4b3d33413d53d4e6de96f2cc027
TheWall <dannywall.2000@gmail.com>
2018-02-07 19:10:33 -0500

    undeleted the dataobjects

---
+++ Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/powerup2018/dataObjects/PostGameObject.java
@@ -0,0 +1,34 @@
+package ca.team2706.scouting.mcmergemanager.powerup2018.dataObjects;
+
+import java.io.Serializable;
+
+/**
+ * Created by dwall on 16/01/17.
+ */
+
+public class PostGameObject implements Serializable {
+    public String notes = "";
+
+    public enum ClimbType {
+        // Success Assisted means they were assisted climbing
+        // Success Assisted Others means they assisted others while climbing
+        NO_CLIMB, FAIL, SUCCESS_INDEPENDENT, SUCCESS_ASSISTED, SUCCESS_ASSISTED_OTHERS;
+    }
+
+    public ClimbType climbType;
+    public double climb_time;
+    public double time_dead;
+    public double time_defending;
+
+
+    // empty constructor
+    public PostGameObject() {}
+
+    public PostGameObject(String notes, ClimbType climbType, double climb_time, double time_dead, double time_defending) {
+        this.climbType = climbType;
+        this.notes = notes;
+        this.climb_time = climb_time;
+        this.time_dead = time_dead;
+        this.time_defending = time_defending;
+    }
+}
