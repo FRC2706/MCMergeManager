ed42f5831467b0a2f09a547fc2e4147adba9e51f
Lara <laralim10@gmail.com>
2018-02-19 12:51:11 -0500

    edited dropped cube fragment

--- Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/powerup2018/DroppedCubeFragment.java
+++ Scouting/src/main/java/ca/team2706/scouting/mcmergemanager/powerup2018/DroppedCubeFragment.java
@@ -4,6 +4,7 @@ import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewGroupCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -45,5 +46,16 @@ public class DroppedCubeFragment extends DialogFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         final DialogFragment me = this;
+
+        view.findViewById(R.id.cancel_button).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Log.i(getClass().getName(), "quit");
+                        listener.editNameDialogCancel(me);
+                    }
+                }
+        );
+
     }
 }
